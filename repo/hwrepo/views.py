e996c94e618248a099e02b3705bb978de2cdfb68
blueSherii <sherif@adjust.com>
2020-09-05 23:05:02 +0200

    Test html response

--- repo/hwrepo/views.py
+++ repo/hwrepo/views.py
@@ -2,10 +2,18 @@
 from __future__ import unicode_literals
 from django.http import HttpRequest
 from django.http import HttpResponse
+from .models import Leaseweb
 
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def main(request):
-    return HttpResponse("Under construction");
+    items = Leaseweb.objects.all()
+    assetsIds = []
+    for server in items:
+        assetsIds.append(str(server.assetId))
+
+    html = '<br>'.join(assetsIds)
+    return HttpResponse(html)
